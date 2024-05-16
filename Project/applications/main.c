784844ed14e9eb6c17a3d2ab95d1dab5d53f48bb
frame-electric <277402960@qq.com>
2024-05-17 01:45:03 +0800

    feat:串口使用uart1,PA9 PA10
    Signed-off-by: frame-electric <277402960@qq.com>

--- Project/applications/main.c
+++ Project/applications/main.c
@@ -12,6 +12,7 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <board.h>
+#include <rtdbg.h>
 
 /* defined the LED0 pin: PF9 */
 #define LED0_PIN    GET_PIN(F, 9)
@@ -27,5 +28,6 @@ int main(void)
         rt_thread_mdelay(500);
         rt_pin_write(LED0_PIN, PIN_LOW);
         rt_thread_mdelay(500);
+        rt_kprintf("Hello RT-Thread!\n");
     }
 }
